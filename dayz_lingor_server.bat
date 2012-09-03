922b3f6b3d6aab2e4a59e495e1ec9e68b909583c
Fox972 <tylerbykerk@yahoo.com>
2012-09-02 21:13:48 -0700

    Update dayz_lingor_server.bat
    Lingor server now loads from main config file

--- dayz_lingor_server.bat
+++ dayz_lingor_server.bat
@@ -7,5 +7,5 @@ cls
 echo "Please wait...Loading"
 ping -n 10 127.0.0.1>nul
 server_setup\perl\bin\perl -w vehicles.pl --cleanup --world lingor 1 dayz root 123456
-"Expansion\beta\arma2oaserver.exe" -port=2302 "-config=Sanctuary\config_lingor.cfg" "-cfg=Sanctuary\basic.cfg" "-profiles=Sanctuary" -name=Sanctuary "-mod=@Arma2NET;@CBA;@CBA_OA;@CBA_A2;@DayZ_lingor;@dayz_lingor_island;@Sanctuary_Lingor;" -cpuCount=4 -maxMem=1578 -exThreads=7
+"Expansion\beta\arma2oaserver.exe" -port=2302 "-config=Sanctuary\config.cfg" "-cfg=Sanctuary\basic.cfg" "-profiles=Sanctuary" -name=Sanctuary "-mod=@Arma2NET;@CBA;@CBA_OA;@CBA_A2;@DayZ_lingor;@dayz_lingor_island;@Sanctuary_Lingor;" -cpuCount=4 -maxMem=1578 -exThreads=7
 cd server_setup\xampp & mysql_stop.bat
